d1dd7b73aeac6dcfd64c09b33f8ccce426de4215
Chen Luo <luochen1007@gmail.com>
2021-01-13 20:33:53 -0500

    improved style & added pie chart

--- script.js
+++ script.js
@@ -520,3 +520,22 @@ function clearEverything(){
     if(confirm("This will clear all the data on this page permanently\n Are you sure?"))
         budgetController.clearLocalData();
 }
+
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
